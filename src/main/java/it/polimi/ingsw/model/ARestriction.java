22ca63bfd4ab1c147fc5ab8b9dd2b1bdab769720
Rita <rita@Rita.local>
2018-04-28 17:17:48 +0200

    ARestriction and Die classes

---
+++ src/main/java/it/polimi/ingsw/model/ARestriction.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.model;
+
+public abstract class ARestriction {
+
+    public abstract boolean isRespected(Die choosenDie);
+}
---
+++ src/main/java/it/polimi/ingsw/model/Die.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw.model;
+
+/**
+ * This class manges the Die object.
+ */
+public class Die {
+
+    /**
+     * The numeric value of the die that wil be set at the moment of its construction.
+     */
+    private int dieValue;
+
+    /**
+     * The color value of the die that wil be set at the moment of its construction.
+     */
+    //private Color dieColor;
+
+    public Die(int dieValue) {
+        setDieValue(dieValue);
+    }
+
+    public int getDieValue() {
+        return dieValue;
+    }
+
+    public void setDieValue(int dieValue) {
+        this.dieValue = dieValue;
+    }
+
+
+
+}
