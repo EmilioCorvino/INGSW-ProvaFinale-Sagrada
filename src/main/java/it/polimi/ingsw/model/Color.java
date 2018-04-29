d24ab68b02d11fed24e00d90566ee690cfdbb799
Rita <rita@Rita.local>
2018-04-29 16:19:53 +0200

    enum Color added

---
+++ src/main/java/it/polimi/ingsw/model/Color.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.model;
+
+/**
+ * This enum lists the possible color that can be used in the game.
+ */
+public enum Color {
+
+    PURPLE,
+    BLUE,
+    GREEN,
+    RED,
+    YELLOW
+}
