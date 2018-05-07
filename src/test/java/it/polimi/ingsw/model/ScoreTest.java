578b3cd56caa65db167af85f789b6b106a2054d9
Emilio <emilio.corvino@mail.polimi.it>
2018-05-07 18:17:49 +0200

    Partial testing of Score class (because the class isn't finished yet).

---
+++ src/test/java/it/polimi/ingsw/model/ScoreTest.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.model;
+
+import org.junit.Test;
+
+public class ScoreTest {
+
+    /**
+     * Checks if lost points are computed correctly.
+     */
+    @Test
+    public void lostPointsTest() {
+        WindowPatternCard wpc = new WindowPatternCard();
+    }
+}
