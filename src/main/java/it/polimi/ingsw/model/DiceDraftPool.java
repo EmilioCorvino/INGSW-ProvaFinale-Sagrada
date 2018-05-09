120ecfb31529455ed6444081c0628867c31bf5ca
GianlucaDrappo <37829458+GianlucaDrappo@users.noreply.github.com>
2018-05-09 15:01:36 +0200

    Added classes DiceDraftPool and DiceDraftPoolTest

--- src/main/java/it/polimi/ingsw/model/DiceDraftPool.java
+++ src/main/java/it/polimi/ingsw/model/DiceDraftPool.java
@@ -9,11 +9,6 @@ import java.util.List;
 public class DiceDraftPool extends ADieContainer{
 
     /**
-     * Number of player that are playing the match.
-     */
-    private int numberOfPlayers;
-
-    /**
      * List of Dice in the bag.
      */
     private List<Die> availableDice;
@@ -23,13 +18,10 @@ public class DiceDraftPool extends ADieContainer{
      */
     private DiceBag diceBag;
 
-
     /**
      * The constructor
      */
-    public DiceDraftPool(int numberOfPlayers){
-
-        this.numberOfPlayers = numberOfPlayers;
+    public DiceDraftPool(){
         this.availableDice = new ArrayList<>();
         this.diceBag = new DiceBag();
     }
@@ -41,21 +33,44 @@ public class DiceDraftPool extends ADieContainer{
     /**
      * This method call the update the DiceDraftPool in each round.
      */
-    public void pooulateDiceDraftPool(){
+    public void populateDiceDraftPool(int numberOfPlayers){
 
         int i = 0;
-        while( diceBag.extract() != null && i < 2*numberOfPlayers + 1){
-            update(diceBag.extract());
+        boolean empty = false;
+        Die dieExtracted;
+
+        while( !empty && i < 2*numberOfPlayers + 1){
+            dieExtracted = diceBag.extract();
+            if (dieExtracted == null)
+                empty = true;
+            else
+                this.getAvailableDice().add(dieExtracted);
             i++;
         }
     }
 
-    //public Die chooseDie(Die die){
+    /**
+     * This method returns the die's chosen.
+     * @param die: A copy of die chosen.
+     * @return The reference at the die chosen
+     */
+    public Die chooseDie(Die die){
+        for( Die d : this.getAvailableDice())
+            if(d.getDieColor() == die.getDieColor() && d.getActualDieValue() == die.getActualDieValue())
+                return d;
+        return null;
+    }
 
-    //}
-
+    /**
+     * This method update the DraftPool list's of dice, removing the die chosen.
+     * @param die: A copy of the die that has to be removed.
+     */
     @Override
     public void update(Die die) {
-        availableDice.add(die);
+        Die dieToBeDeleted = null;
+        for( Die d : this.getAvailableDice())
+            if(d.getDieColor() == die.getDieColor() && d.getActualDieValue() == die.getActualDieValue())
+                dieToBeDeleted = d;
+        this.getAvailableDice().remove(dieToBeDeleted);
     }
 }
--- src/test/java/it/polimi/ingsw/model/DiceDraftPoolTest.java
+++ src/test/java/it/polimi/ingsw/model/DiceDraftPoolTest.java
@@ -2,19 +2,38 @@ package it.polimi.ingsw.model;
 
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
 public class DiceDraftPoolTest {
 
+    /**
+     * This test verify the correct population of the DraftPool
+     */
     @Test
-    public void getAvailableDice() {
+    public void populateDiceDraftPool() {
+        DiceDraftPool diceDraftPool = new DiceDraftPool();
+        int numberOfPlayer = 4;
+
+        diceDraftPool.populateDiceDraftPool(numberOfPlayer);
+
+        assertEquals(numberOfPlayer*2 +1, diceDraftPool.getAvailableDice().size());
     }
 
+    /**
+     * This test verify if an update of a Draft pool effective remove a die from the Draft.
+     */
     @Test
-    public void pooulateDiceDraftPool() {
-    }
+    public void checkUpdate() {
+        DiceDraftPool diceDraftPool = new DiceDraftPool();
+        int numberOfPlayer = 4;
 
-    @Test
-    public void update() {
+        diceDraftPool.populateDiceDraftPool(numberOfPlayer);
+        Die die = diceDraftPool.getAvailableDice().get(new Random().nextInt(diceDraftPool.getAvailableDice().size()));
+
+        diceDraftPool.update(die);
+        assertEquals( numberOfPlayer*2 + 1 - 1 , diceDraftPool.getAvailableDice().size());
+
     }
 }
