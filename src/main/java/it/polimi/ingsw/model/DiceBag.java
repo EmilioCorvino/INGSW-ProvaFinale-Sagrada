f821f06284d80d38c26c199742ab7ddbee903379
GianlucaDrappo <37829458+GianlucaDrappo@users.noreply.github.com>
2018-05-09 15:01:11 +0200

    Added classes DiceBag and DiceBagTest

--- src/main/java/it/polimi/ingsw/model/DiceBag.java
+++ src/main/java/it/polimi/ingsw/model/DiceBag.java
@@ -17,30 +17,41 @@ public class DiceBag extends ADieContainer{
     /**
      * Number of dice for color.
      */
-    private static final int dieNumberForEachColor = 18;
+    private static final int DICE_NUMBER_FOR_EACH_COLOR = 18;
 
     /**
      * Number of colors available.
      */
-    private static final int numberOfColor = 5;
+    private static final int NUMBER_OF_COLORS = 5;
 
     /**
      * The constructor create the list of die and populates it with some dies for each color, with a random value.
      */
     public DiceBag(){
         this.availableDice = new ArrayList<>();
-        for(int i = 0; i < numberOfColor; i++)
-            for (int j = 0; j< dieNumberForEachColor; j++)
-                availableDice.add(new Die(1 + new Random().nextInt(6) , Color.values()[i]));
+        for(int i = 0; i < NUMBER_OF_COLORS; i++)
+            for (int j = 0; j< DICE_NUMBER_FOR_EACH_COLOR; j++)
+                availableDice.add(new Die(1 + new Random().nextInt(6), Color.values()[i]));
     }
 
+    public List<Die> getAvailableDice() {
+        return availableDice;
+    }
+
+    public static int getDieNumberForEachColor() {
+        return DICE_NUMBER_FOR_EACH_COLOR;
+    }
+
+    public static int getNumberOfColors() {
+        return NUMBER_OF_COLORS;
+    }
 
     /**
      * This method extract a Die from the List of available Dice, and remove it from there.
-     * @return The die removed from the bag, with a random color.
+     * @return The die random removed from the bag.
      */
     public Die extract(){
-        if(availableDice.size() != 0) {
+        if(!availableDice.isEmpty()) {
             Die dieToBeExtracted = availableDice.get((new Random()).nextInt(90));
             update(dieToBeExtracted);
             return dieToBeExtracted;
--- src/test/java/it/polimi/ingsw/model/DiceBagTest.java
+++ src/test/java/it/polimi/ingsw/model/DiceBagTest.java
@@ -6,12 +6,59 @@ import static org.junit.Assert.*;
 
 public class DiceBagTest {
 
+    /**
+     * This test check if at the construction the dice bag is populated with the correct number of die for each color.
+     */
     @Test
-    public void extract() {
+    public void checkPopulation(){
+        DiceBag diceBag = new DiceBag();
 
+        int red = 0;
+        int blue = 0;
+        int purple = 0;
+        int yellow = 0;
+        int green = 0;
+
+        for(Die d : diceBag.getAvailableDice()){
+            if (d.getDieColor() == Color.RED)
+                red ++;
+            if (d.getDieColor() == Color.BLUE)
+                blue ++;
+            if (d.getDieColor() == Color.PURPLE)
+                purple ++;
+            if (d.getDieColor() == Color.YELLOW)
+                yellow ++;
+            if (d.getDieColor() == Color.GREEN)
+                green ++;
+        }
+
+        assertEquals(diceBag.getDieNumberForEachColor()*diceBag.getNumberOfColors(), diceBag.getAvailableDice().size());
+
+        assertEquals(diceBag.getDieNumberForEachColor(), red);
+        assertEquals(diceBag.getDieNumberForEachColor(), blue);
+        assertEquals(diceBag.getDieNumberForEachColor(), purple);
+        assertEquals(diceBag.getDieNumberForEachColor(), yellow);
+        assertEquals(diceBag.getDieNumberForEachColor(), green);
     }
 
+    /**
+     * This test verify if the extraction effective remove a die from the list, so the list contains one die less.
+     * And it also verify if the dice inside tha bag, with the same color of the die extracted, are one less.
+     */
     @Test
-    public void update() {
+    public void extractDie() {
+        DiceBag diceBag = new DiceBag();
+
+        int nDiceSameColor = 0;
+
+        Die die = diceBag.extract();
+
+        assertTrue((diceBag.getDieNumberForEachColor()*diceBag.getNumberOfColors() - 1) == diceBag.getAvailableDice().size());
+
+        for (Die d : diceBag.getAvailableDice())
+            if(die.getDieColor() == d.getDieColor())
+                nDiceSameColor ++;
+
+        assertEquals(diceBag.getDieNumberForEachColor() - 1, nDiceSameColor);
     }
 }
