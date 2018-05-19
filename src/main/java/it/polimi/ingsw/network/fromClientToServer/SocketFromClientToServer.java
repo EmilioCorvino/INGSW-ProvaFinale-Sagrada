6e1ad0370ee196e5700ca4815543cc3fb239ae82
GianlucaDrappo <37829458+GianlucaDrappo@users.noreply.github.com>
2018-05-19 12:47:47 +0200

    Network interfaces added

---
+++ src/main/java/it/polimi/ingsw/network/fromClientToServer/SocketFromClientToServer.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.network.fromClientToServer;
+
+public class SocketFromClientToServer implements IFromClientToServer {
+    @Override
+    public void login(String playerName, String ip, int port, String gameMode) {
+
+    }
+}
