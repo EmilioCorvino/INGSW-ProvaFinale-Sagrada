092a5d99153f0139b3a9177360e0151ae7a57095
GianlucaDrappo <37829458+GianlucaDrappo@users.noreply.github.com>
2018-05-20 15:16:26 +0200

    Add triggerMethod in the common board

--- src/main/java/it/polimi/ingsw/model/CommonBoard.java
+++ src/main/java/it/polimi/ingsw/model/CommonBoard.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.model;
 
 import it.polimi.ingsw.model.cards.PublicObjectiveCardSlot;
 import it.polimi.ingsw.model.cards.ToolCardSlot;
+import it.polimi.ingsw.model.die.Die;
 import it.polimi.ingsw.model.die.diecontainers.DiceDraftPool;
 import it.polimi.ingsw.model.die.diecontainers.RoundTrack;
 import it.polimi.ingsw.model.player.Player;
@@ -14,6 +15,10 @@ import java.util.List;
  */
 //todo This will probably be the observed class that will notify changes.
 public class CommonBoard {
+
+    /*todo This will be the controller connection.
+    ControllerMaster controller;
+    */
 
     public static final int NUMBER_OF_ROUNDS = 10;
     /**
@@ -50,6 +55,53 @@ public class CommonBoard {
         this.publicObjectiveCardSlots = new ArrayList<>();
         this.toolCardSlots = new ArrayList<>();
     }
+
+    /*todo This method will update the Window pattern of a specific player.
+    public void triggerWpPlacement(Die die, Player player){
+        for( Player p : players)
+            if (player.getPlayerName() == p.getPlayerName()){
+                p.getWindowPatternCard().update(die);
+                this.notifyController();
+            }
+
+    }
+    */
+
+    /**
+     * This method make the update of Round Track and trigger the notify to the controller
+     * @param action: Type of modification, (update) if a die want to be added, (remove) if a die want to be removed.
+     * @param die: the die to be added or removed from the round track.
+     */
+    /*todo this method will be update the Round Track and trigger the notify to the controller
+    public void triggerRoundTrackPlacement(String action, Die die){
+        if (action == "update")
+            roundTrack.update(die);
+        if (action == "remove")
+            roundTrack.remove(die);
+        this.notifyController();
+    }
+    */
+
+    /**
+     * This method update the draft pool.
+     * @param die: The die to be added.
+     */
+    /*todo
+    public void triggerDiceDraftPlacement(Die die){
+        draftPool.update(die);
+        this.notifyController();
+    }
+    */
+
+    /**
+     * This method notify the controller that something has been modify.
+    */
+    /*todo This method will notify the modification to the controller.
+    public void notifyController(){
+        controller.updateView();
+    }
+    */
+
 
     public List<Player> getPlayers() {
         return players;
