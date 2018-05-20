5c701cb2a758ad6153e548a316e6d18b115d51d6
GianlucaDrappo <37829458+GianlucaDrappo@users.noreply.github.com>
2018-05-20 15:08:41 +0200

    Added windowPatternDeck class

---
+++ src/main/java/it/polimi/ingsw/model/die/diecontainers/WindowPatternCardDeck.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.model.die.diecontainers;
+
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+public class WindowPatternCardDeck {
+
+    /*
+    public List<WindowPatternCard> getDeck() {
+        return (List<WindowPatternCard>) this.deck;
+    }
+    */
+
+    /*
+    public void parseDeck() {
+        Gson gson = new Gson();
+        try(Reader file = new FileReader("./src/main/resources/cards/windowPatternCard.json")){
+            this.deck = gson.fromJson(file, WindowPatternCard);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    */
+
+}
