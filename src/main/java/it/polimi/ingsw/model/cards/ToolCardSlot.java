2ac2ef31de8914e57ac1f3b24a4288aa42cba0e4
Emilio <emilio.corvino@mail.polimi.it>
2018-05-20 12:36:09 +0200

    Added specific card slots for Public Objective cards and Tool cards.

---
+++ src/main/java/it/polimi/ingsw/model/cards/ToolCardSlot.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.model.cards;
+
+import it.polimi.ingsw.model.cards.tool.IToolCard;
+
+/**
+ * A class that contains a {@link it.polimi.ingsw.model.cards.tool.IToolCard}.
+ * Card slots are fetched instead of the card itself, to be sure the card is available in the specific match.
+ */
+public class ToolCardSlot {
+
+    /**
+     * {@link IToolCard} contained in the slot.
+     */
+    private final IToolCard toolCard;
+
+    public ToolCardSlot(IToolCard card) {
+        this.toolCard = card;
+    }
+
+    public IToolCard getToolCard() {
+        return toolCard;
+    }
+}
