7659f5cdcea26a5c5cffab123c2aa33a18f570fa
GianlucaDrappo <37829458+GianlucaDrappo@users.noreply.github.com>
2018-05-22 14:37:46 +0200

    Updates in Cli

---
+++ src/main/java/it/polimi/ingsw/view/cli/InputOutputManager.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.view.cli;
+
+import java.util.Scanner;
+
+/**
+ * This class is use to menage the input output communication with the user.
+ */
+public class InputOutputManager {
+
+    public String askInformation(String input){
+        this.print(input);
+        return this.read();
+    }
+
+    public String read(){
+        Scanner scan = new Scanner(System.in);
+        return scan.next().trim();
+    }
+
+    public void print(String string){
+        System.out.println(string);
+    }
+
+}
