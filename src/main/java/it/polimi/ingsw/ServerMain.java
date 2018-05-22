3ea5c52a01b2502d88d7e08c262df6b3f2dc3930
Emilio <emilio.corvino@mail.polimi.it>
2018-05-22 17:57:29 +0200

    Further network implementation.

--- src/main/java/it/polimi/ingsw/ServerMain.java
+++ src/main/java/it/polimi/ingsw/ServerMain.java
@@ -10,25 +10,29 @@ import java.rmi.server.UnicastRemoteObject;
 public class ServerMain {
 
     public static final int PORT = 54242;
-    //private final ControllerMaster controllerMaster;
+    private final ControllerMaster controllerMaster;
 
-    /*private ServerMain(ControllerMaster controllerMaster) {
+    private ServerMain(ControllerMaster controllerMaster) {
         this.controllerMaster = controllerMaster;
-    }*/
+    }
 
     public static void main(String[] args) {
-        //ServerMain serverMain = new ServerMain(new ControllerMaster());
+        ServerMain serverMain = new ServerMain(new ControllerMaster());
         //Sets up Rmi server and client remote interfaces.
-        try {
-            RmiServer rmiServer = new RmiServer(new ControllerMaster());
-            IServer stub = (IServer) UnicastRemoteObject.exportObject(rmiServer, PORT);
-            Registry registry = LocateRegistry.createRegistry(PORT);
-            registry.bind("IServer", stub);
-        } catch (Exception e) {
-            System.err.println("Rmi server exception: " + e.toString());
-            e.printStackTrace();
-        }
-        System.err.println("Rmi Server ready, waiting for connections...");
+        Runnable rmiServerThread = () -> {
+                try {
+                    RmiServer rmiServer = new RmiServer(serverMain.controllerMaster);
+                    IServer stub = (IServer) UnicastRemoteObject.exportObject(rmiServer, PORT);
+                    Registry registry = LocateRegistry.createRegistry(PORT);
+                    registry.bind("IServer", stub);
+                } catch (Exception e) {
+                    System.err.println("Rmi server exception: " + e.toString());
+                    e.printStackTrace();
+                }
+                System.err.println("Rmi Server ready, waiting for connections...");
+        };
+        new Thread(rmiServerThread).start();
+
 
         //todo setup socket server
     }
