2d2700e50a31ef4959281531c00e7b56cdbe9d86
GianlucaDrappo <37829458+GianlucaDrappo@users.noreply.github.com>
2018-05-22 18:03:36 +0200

    Add scheleton of parser

---
+++ src/main/java/it/polimi/ingsw/ParserMain.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.model.die.diecontainers.WindowPatternCardDeck;
+
+public class ParserMain {
+
+    public static void main(String[] args){
+
+        WindowPatternCardDeck wpDeck = new WindowPatternCardDeck();
+        wpDeck.parseWP();
+    }
+}
