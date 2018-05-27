2bdf7e690ed00e28fa243058ef9aaaec723023b2
rita <rita.adesso@mail.polimi.it>
2018-05-27 19:02:32 +0200

    waiting room and connection classes added

---
+++ src/main/java/it/polimi/ingsw/controller/WaitingRoom.java
@@ -0,0 +1,48 @@
+package it.polimi.ingsw.controller;
+
+import it.polimi.ingsw.network.Connection;
+import it.polimi.ingsw.utils.exceptions.TooManyUsersException;
+import it.polimi.ingsw.utils.exceptions.UserNameAlreadyTakenException;
+
+import java.util.Map;
+
+/**
+ * This class manages the waiting player before the match starts.
+ */
+public class WaitingRoom {
+
+    /**
+     * Maximum number of players that can connect to the match.
+     */
+    static final int MAX_PLAYERS = 4;
+
+    /**
+     *
+     */
+    Map<String, Connection> playersRoom;
+
+    /**
+     *
+     */
+    boolean running;
+
+    /**
+     * This method adds a player to the waiting room.
+     * @param username the name of the player to add.
+     * @param connection the connection of the player to add.
+     * @throws UserNameAlreadyTakenException
+     * @throws TooManyUsersException
+     */
+    public void addPlayer(String username, Connection connection) throws UserNameAlreadyTakenException, TooManyUsersException {
+
+        playersRoom.entrySet().forEach( entry -> {
+            if(username.equals(entry.getKey()))
+                throw new UserNameAlreadyTakenException();
+
+            if(playersRoom.size() == MAX_PLAYERS)
+                throw new TooManyUsersException();
+        });
+
+        playersRoom.put(username, connection);
+    }
+}
