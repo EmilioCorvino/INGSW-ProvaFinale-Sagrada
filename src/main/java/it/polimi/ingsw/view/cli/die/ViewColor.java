eeaeb4dc44e0d215d484dd89d144c2f29bfa6d71
GianlucaDrappo <37829458+GianlucaDrappo@users.noreply.github.com>
2018-05-27 12:16:57 +0200

    Add javaDoc in enum ViewColor

--- src/main/java/it/polimi/ingsw/view/cli/die/ViewColor.java
+++ src/main/java/it/polimi/ingsw/view/cli/die/ViewColor.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.view.cli.die;
 
+/**
+ * This enum contains the range of colors that a die can have.
+ */
 public enum ViewColor {
     PURPLE(35,"P"),
     BLUE(34,"B"),
@@ -7,7 +10,14 @@ public enum ViewColor {
     RED(31,"R"),
     YELLOW(33,"Y");
 
+    /**
+     * It is the number needed to print a string in this color
+     */
     private int colorNumber;
+
+    /**
+     * It is the id of the color, to recognize a color from an other.
+     */
     private String id;
 
     public int getColorNumber() {
