48ac5a38c89a0257bcd833a9b7e7fe817bd12baf
rita <rita.adesso@mail.polimi.it>
2018-05-27 21:23:39 +0200

    waiting room updated with addPlayer, checkNumberOfPlayer and login methods

--- src/main/java/it/polimi/ingsw/controller/WaitingRoom.java
+++ src/main/java/it/polimi/ingsw/controller/WaitingRoom.java
@@ -5,6 +5,8 @@ import it.polimi.ingsw.utils.exceptions.TooManyUsersException;
 import it.polimi.ingsw.utils.exceptions.UserNameAlreadyTakenException;
 
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * This class manages the waiting player before the match starts.
@@ -36,30 +38,53 @@ public class WaitingRoom {
      */
     boolean running;
 
+    public void login(String username, Connection connection, String gameMode) throws UserNameAlreadyTakenException, TooManyUsersException {
+
+        if(Integer.parseInt(gameMode) == SINGLEPLAYER_MODE)
+            startSingleMatch();
+        else if(Integer.parseInt(gameMode) == MULTIPLAYER_MODE) {
+
+            if (playersRoom.size() == MAX_PLAYERS)
+                throw new TooManyUsersException();
+
+            addPlayer(username, connection);
+        }
+
+        checkNumberOfPlayers();
+
+    }
+
+    /**
+     * This method checks if the minimum player for a multiplayer match is reached, then starts the timer.
+     */
+    private void checkNumberOfPlayers() {
+
+        if(playersRoom.size() == 2) {
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    System.out.println("timer is expired");
+                    startMultiPlayerMatch();
+                }
+            }, 5 * 1000);
+        }
+    }
+
     /**
      * This method adds a player to the waiting room.
      * @param username the name of the player to add.
      * @param connection the connection of the player to add.
      * @throws UserNameAlreadyTakenException
-     * @throws TooManyUsersException
      */
-    public void addPlayer(String username, Connection connection, int gameMode) throws UserNameAlreadyTakenException, TooManyUsersException {
-        
-            if(gameMode == SINGLEPLAYER_MODE)
-                startSingleMatch();
-            else if(gameMode == MULTIPLAYER_MODE) {
+    public void addPlayer(String username, Connection connection) throws UserNameAlreadyTakenException {
 
+        for (String name : playersRoom.keySet())
+            if (username.equals(name))
+                throw new UserNameAlreadyTakenException();
 
-                for (String name : playersRoom.keySet())
-                    if (username.equals(name))
-                        throw new UserNameAlreadyTakenException();
+        playersRoom.put(username, connection);
 
-                if (playersRoom.size() == MAX_PLAYERS)
-                    throw new TooManyUsersException();
-
-                playersRoom.put(username, connection);
-
-            }
     }
 
     public Map<String, Connection> getPlayersRoom() {
@@ -68,6 +93,13 @@ public class WaitingRoom {
 
     public void setPlayersRoom(Map<String, Connection> playersRoom) {
         this.playersRoom = playersRoom;
+    }
+
+    /**
+     *
+     */
+    public void startMultiPlayerMatch() {
+
     }
 
     /**
