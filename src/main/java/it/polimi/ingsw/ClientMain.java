d22a4c7be2ebc1c766cad7c6c34863536d1669bf
rita <rita.adesso@mail.polimi.it>
2018-06-17 15:12:33 +0200

    choose type of view commands added in ClientMain

--- src/main/java/it/polimi/ingsw/ClientMain.java
+++ src/main/java/it/polimi/ingsw/ClientMain.java
@@ -1,7 +1,11 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.view.GUI.GUIMain;
 import it.polimi.ingsw.view.IViewMaster;
 import it.polimi.ingsw.view.cli.CliView;
+import javafx.application.Application;
+
+import java.util.Scanner;
 
 /**
  * This class is used to launch a client that uses the command line interface.
@@ -10,7 +14,26 @@ import it.polimi.ingsw.view.cli.CliView;
 public class ClientMain {
 
     public static void main(String[] args) {
-        IViewMaster viewMaster = new CliView();
-        viewMaster.createConnection(viewMaster);
+
+        System.out.println("Scegli il tipo di view: -cli-  o -gui-");
+        Scanner scan = new Scanner(System.in);
+        String code = scan.next();
+
+        while(!(code.equals("cli") || code.equals("gui"))) {
+            System.out.println("Scegli il tipo di view: -cli-  o -gui-");
+            code = scan.next();
+        }
+
+        if(code.equals("cli")) {
+            IViewMaster viewMaster = new CliView();
+            viewMaster.createConnection(viewMaster);
+        }
+
+        if(code.equals("gui")) {
+            GUIMain gui = new GUIMain();
+            Application.launch(gui.getClass(), args);
+
+        }
+
     }
 }
