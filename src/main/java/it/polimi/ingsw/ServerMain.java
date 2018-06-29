afb350ceb961cfa153d91e7d36e04faf6c3e4c10
Emilio <emilio.corvino@mail.polimi.it>
2018-06-29 18:32:25 +0200

    bug fix.

--- src/main/java/it/polimi/ingsw/ServerMain.java
+++ src/main/java/it/polimi/ingsw/ServerMain.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.FileAlreadyExistsException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.logging.Level;
@@ -21,7 +20,7 @@ public class ServerMain {
 
     private WaitingRoom room; //Substitute this with a match handler to handle more matches.
 
-    public static final String RMI_PORT_PATH = "./src/main/java/it/polimi/ingsw/utils/config/rmiPort";
+    public static final String RMI_PORT_PATH = "./src/main/resources/config/rmiPort";
 
     private ServerMain(WaitingRoom room) {
         this.room = room;
